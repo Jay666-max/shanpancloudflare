9e8f0be2415345d136365abbbe60f757ad203c9c
huojian51 <401013770@qq.com>
2025-01-27 01:56:13 +0800

    Update vite.config.ts base path configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.CF_PAGES ? '/' : '/cloudflare-shapan/',
+  base: process.env.CF_PAGES ? '/' : '/shanpancloudflare/',
 })
