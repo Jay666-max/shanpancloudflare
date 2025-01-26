a362121c1d145536e871b2bc5957bf92f26cab6f
huojian51 <401013770@qq.com>
2025-01-27 02:16:22 +0800

    更新 vite.config.ts 的 base 配置为 /shanpancloudflare/

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: process.env.CF_PAGES ? '/' : '/shanpancloudflare/',
+  base: '/shanpancloudflare/',
+  plugins: [react()]
 })
